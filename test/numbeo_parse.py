a5c18597bfbf9dd0971eb04d92192c1aad011d73
Bass <bass@Basss-MacBook-Pro.local>
2018-01-03 18:12:07 +0200

    Estimator initial commit

---
+++ test/numbeo_parse.py
@@ -0,0 +1,21 @@
+from HTMLParser import HTMLParser
+
+link="https://www.numbeo.com/cost-of-living/city-estimator/in/{city}?displayCurrency=EUR" \
+"&members=4&restaurants_percentage=10.0&inexpensive_restaurants_percentage=50.0" \
+"&drinking_coffee_outside=100.0&going_out_monthly=4.2&smoking_packs_per_day=0.0" \
+"&alcoholic_drinks=25.0&type_of_food=0&driving_car=0.0&taxi_consumption=0.0" \
+"&paying_for_public_transport=Monthly%2C+All+Members&sport_memberships=100.0" \
+"&vacation=0.0&clothing_and_shoes=50.0&rent=26&displayCurrency=EUR&members=4" \
+"&restaurants_percentage=10.0&inexpensive_restaurants_percentage=50.0" \
+"&drinking_coffee_outside=100.0&going_out_monthly=4.2&smoking_packs_per_day=0.0" \
+"&alcoholic_drinks=25.0&type_of_food=0&driving_car=0.0&taxi_consumption=0.0" \
+"&paying_for_public_transport=Monthly%2C+All+Members&sport_memberships=100.0" \
+"&vacation=0.0&clothing_and_shoes=50.0&rent=26"
+
+cities = [ 'Tallinn', 'Kiev', 'New-York', 'Helsinki' ]
+
+#parser = MyHTMLParser()
+
+for city in cities:
+    print city
+    print link.format(city=city)
