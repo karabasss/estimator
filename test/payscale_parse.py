4c09a006a1c41c835cf963880c9f0afe753c52c4
bass <karabass.com@gmail.com>
2018-01-11 12:47:54 +0200

    Payscale test list od dicts

--- test/payscale_parse.py
+++ test/payscale_parse.py
@@ -8,7 +8,10 @@ link = "https://www.payscale.com/research/{country}/Job=Software_Developer/Salar
 
 cities = [
     {'city': 'Tallinn', 'country': 'EE', 'ps_code': '75b81c34'},
+    {'city': 'Kiev', 'country': 'UA', 'ps_code': '221d38a6'},
     {'city': 'New-York-NY', 'country': 'US', 'ps_code': '5dda9200'},
-
+    {'city': 'Helsinki', 'country': 'FI', 'ps_code': '01b00af0'}
 ]
 
+for city in cities:
+    print link.format(city=city['city'],country=city['country'],code=city['ps_code'])
